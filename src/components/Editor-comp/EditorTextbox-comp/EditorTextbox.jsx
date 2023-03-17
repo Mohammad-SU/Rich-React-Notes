da75d5f4d512151ea22b3e98c33934f41949b8f5
NeonTech707 <LearTec4304@gmail.com>
2023-03-17 02:35:20 +0000

    Fixed some editor textbox sizing

--- src/components/Editor-comp/EditorTextbox-comp/EditorTextbox.jsx
+++ src/components/Editor-comp/EditorTextbox-comp/EditorTextbox.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
 import './EditorTextbox.css'
 
 export default function EditorTextbox({ valueMain, onChangeMain, valueTitle, onChangeTitle }) {
     return (
-        <div>
-            <textarea 
-                className="EditorMainTextbox textboxStyle" 
-                maxLength={40000}
-                placeholder="Enter text..."
-                value={valueMain}
-                onChange={onChangeMain}
-            ></textarea>
+        <div className="EditorTextbox">
             <textarea 
                 className="EditorTitle textboxStyle" 
                 maxLength={30}
                 placeholder="Enter title..."
                 value={valueTitle}
                 onChange={onChangeTitle}
+            ></textarea>
+            <textarea 
+                className="EditorMainTextbox textboxStyle" 
+                maxLength={40000}
+                placeholder="Enter text..."
+                value={valueMain}
+                onChange={onChangeMain}
             ></textarea>
         </div>
     )
