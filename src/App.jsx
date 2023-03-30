43bc96aca4e10cf70ba2a3f19643a2fbfbb4f858
NeonTech707 <mohammad.su3@outlook.com>
2023-03-30 19:32:24 +0100

    Added media and image upload features for editor

--- src/App.jsx
+++ src/App.jsx
@@ -140,10 +140,7 @@ export default function App() {
 		}
 
 		setShowEditor(current => !current) // showEditor bool is changed to opposite value each click.
-
 	}
-	
-
 
 	return (
 		<div className="App">
@@ -152,7 +149,7 @@ export default function App() {
 				<NotesList notes={notes} onNoteClick={handleNoteClick}/>
 				<Editor
 					visibleCheck={showEditor}
-					tileText={editorNoteTitle} 
+					titleText={editorNoteTitle} 
 					mainText={editorNoteMain} 
 					onChangeMain={handleChangeMain}
 					onChangeTitle={handleChangeTitle}
@@ -170,13 +167,17 @@ export default function App() {
 }
 
 	/* TODO:
-		Change css for editor element (title margin bottom, background colour ckeditor, etc.)
+		Test deletion of ckeditor5 top level file
+		Give "image_resized" class to all img elements in note main area?
+		Manually edit css of images in note main area to reflect editor layout
+		editor toolbar custom colours (lime, light blue, etc)
+		Change css for editor element (background colour ckeditor)
 		Folders, with colours for each
 		Search (by note title, text, and/or date),
+		Sort? date modified, date created, title, a-z, latest first/latest last
 		Make notes save to local storage
 		Add extra options below note: + animation for it
 			Delete, Duplicate, Copy, Favourites, Pin
 		Dark mode
-		Custom note backgrounds/insert image
-		Change import speech marks to double
+		Custom note backgrounds?
 	*/
