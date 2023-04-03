44ec6a82ad2ea95b3caa52398d069582970de708
NeonTech707 <mohammad.su3@outlook.com>
2023-04-03 10:35:13 +0100

    test fix

--- src/components/Editor-folder/EditorTextbox-comp/EditorTextbox.jsx
+++ src/components/Editor-folder/EditorTextbox-comp/EditorTextbox.jsx
@@ -1,7 +1,7 @@
 import './EditorTextbox.css'
 import { memo } from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import Editor from 'node_modules/ckeditor5-custom-build/build/ckeditor';
 
 function EditorTextbox({ mainText, onChangeMain, titleText, onChangeTitle }) {
 
