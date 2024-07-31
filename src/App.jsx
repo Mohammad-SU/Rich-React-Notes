34919376c5d73ecf757fbce08c72d92786d9a4a2
NeonTech707 <mohammad.su3@outlook.com>
2024-07-31 04:00:56 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,11 @@ import Editor from './components/Editor-folder/Editor-comp/Editor'
 import Warning from './components/Warning-comp/Warning'
 
 function App() {
-	const versionNum = 6; // Update version when noteExamples are changed
+	const versionNum = 7; // Update version when noteExamples are changed
 	const [version, setVersion] = useLocalStorage("version", 0)
 
 	useEffect(() => {
 		if (versionNum > version) { // Cause user's notes to reset to default examples if true
-			localStorage.removeItem("version");
 			localStorage.removeItem("notesData");
 			setVersion(versionNum);
 			location.reload();
